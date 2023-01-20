562fadff481f85363344507053f1f374d9b128d1
dani_mk49 <danielalvarorio@gmail.com>
2023-01-20 17:12:37 +0100

    logica y fichas

--- app/src/main/java/com/example/tg2grupo1/modelo/Fichas.java
+++ app/src/main/java/com/example/tg2grupo1/modelo/Fichas.java
@@ -1,5 +1,22 @@
 package com.example.tg2grupo1.modelo;
 
 public class Fichas {
+    private int id;
+    private String tipo;
 
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getTipo() {
+        return tipo;
+    }
+
+    public void setTipo(String tipo) {
+        this.tipo = tipo;
+    }
 }
