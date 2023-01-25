e793fbe006d8eb281a0328c4e05c7a216a4b1823
unknown <mikeliza2001@outlook.es>
2023-01-25 16:32:50 +0100

    dialogo listo

--- app/src/main/java/com/example/tg2grupo1/vista/Inicio.java
+++ app/src/main/java/com/example/tg2grupo1/vista/Inicio.java
@@ -1,16 +1,46 @@
 package com.example.tg2grupo1.vista;
 
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.DialogInterface;
 import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
 import com.example.tg2grupo1.R;
 
 public class Inicio extends AppCompatActivity {
 
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_inicio);
+
+        Button Creditos = findViewById(R.id.btnCreditos);
+        Creditos.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                mostrarAlertDialog();
+            }
+        });
+    }
+
+
+
+        public void mostrarAlertDialog(){
+            AlertDialog.Builder dialogo = new AlertDialog.Builder(this);
+            LayoutInflater inflater = this.getLayoutInflater();
+            dialogo.setTitle("");
+
+            View dialogoView = inflater.inflate(R.layout.dialogocreditos, null);
+            dialogo.setView(dialogoView);
+
+
+            dialogo.show();
     }
 }
--- app/src/main/java/com/example/tg2grupo1/vista/MainActivity.java
+++ app/src/main/java/com/example/tg2grupo1/vista/MainActivity.java
@@ -2,9 +2,14 @@ package com.example.tg2grupo1.vista;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
 
 import com.example.tg2grupo1.R;
+
+import java.util.Timer;
+import java.util.TimerTask;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -12,5 +17,17 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        TimerTask tarea = new TimerTask() {
+            @Override
+            public void run() {
+                Intent intent = new Intent(MainActivity.this, Inicio.class);
+                startActivity(intent);
+                finish();
+            }
+        };
+
+        Timer tiempo = new Timer();
+        tiempo.schedule(tarea, 3000);
     }
 }
