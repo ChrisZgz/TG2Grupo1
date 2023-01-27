4805aba24c96b70d3f4648d957e16dff876dbe8b
Nicolás Del Amo <nico.delamo.perez@gmail.com>
2023-01-27 23:29:11 +0100

    File

---
+++ app/src/main/java/com/example/tg2grupo1/controlers/LecturaEscrituraDatos.java
@@ -0,0 +1,16 @@
+package com.example.tg2grupo1.controlers;
+
+import android.content.Context;
+
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.PrintWriter;
+
+public class LecturaEscrituraDatos {
+
+    public void registrar(){
+        File directorio = new File ("directorio");
+        
+    }
+
+}
