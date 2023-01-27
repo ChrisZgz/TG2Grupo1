9add2fb208dca05ca1872dbad2e0c60440f63dda
Nicolás Del Amo <nico.delamo.perez@gmail.com>
2023-01-27 17:38:00 +0100

    Gif

--- app/src/main/java/com/example/tg2grupo1/vista/MainActivity.java
+++ app/src/main/java/com/example/tg2grupo1/vista/MainActivity.java
@@ -5,18 +5,25 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.os.Handler;
+import android.webkit.WebView;
+import android.widget.ImageView;
 
+import com.bumptech.glide.Glide;
 import com.example.tg2grupo1.R;
 
 import java.util.Timer;
 import java.util.TimerTask;
 
 public class MainActivity extends AppCompatActivity {
-
+    ImageView gif1;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        gif1 = findViewById(R.id.gif);
+
+        Glide.with(this).load(R.drawable.partidasplash).into(gif1);
 
         TimerTask tarea = new TimerTask() {
             @Override
