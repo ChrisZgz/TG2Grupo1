d94ca008ca27ae9b55a73afa0a4d8ca5cd7ac9a4
dani <danielalvarorio@gmail.com>
2023-01-27 02:43:00 +0100

    adapter juego
    clase adapter para la visualizacion del recyler view del juego, con funcion onclick
    Falta por hacer el layout que carga el recycler con los datos, por lo que tambien falta el holder que indica donde tiene que cargar el dato

--- app/src/main/java/com/example/tg2grupo1/adapter/JuegoAdapter.java
+++ app/src/main/java/com/example/tg2grupo1/adapter/JuegoAdapter.java
@@ -1,4 +1,63 @@
 package com.example.tg2grupo1.adapter;
 
-public class JuegoAdapter {
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.example.tg2grupo1.modelo.Fichas;
+
+import java.util.List;
+
+public class JuegoAdapter extends RecyclerView.Adapter<JuegoAdapter.ViewHolder> {
+    private List<Fichas> fichas:
+    private ViewHolder.OnNoteListener mOnNoteListener;
+
+    public JuegoAdapter(List<Fichas> fichas, ViewHolder.OnNoteListener onNoteListener) {
+        this.fichas = fichas;
+        this.mOnNoteListener = onNoteListener;
+    }
+
+    @NonNull
+    @Override
+    public ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        LayoutInflater inflater = LayoutInflater.from(parent.getContext());
+        //FALTA CREAR EL CARD O LAYOUT que cargue el recycler view cada vez que
+        //meta un dato
+        View view = inflater.inflate(R.layout, parent, false);
+        return new ViewHolder(view, mOnNoteListener);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull ViewHolder holder, int position) {
+        //aqui hay que poner el image view desde el modelo
+        //holder.titulo.setText(series.get(position).getTitulo());     EJEMPLO
+        holder.
+    }
+
+    @Override
+    public int getItemCount() {
+        return fichas.size();
+    }
+
+    public static class ViewHolder extends RecyclerView.ViewHolder implements View.OnClickListener {
+        OnNoteListener onNoteListener;
+
+        public ViewHolder(@NonNull View itemView, OnNoteListener onNoteListener) {
+            super(itemView);
+            this.onNoteListener = onNoteListener;
+            itemView.setOnClickListener(this);
+        }
+
+        @Override
+        public void onClick(View view) {
+            onNoteListener.onNoteClic(getAdapterPosition());
+        }
+
+        public interface OnNoteListener {
+            void onNoteClic(int posicion);
+        }
+    }
 }
